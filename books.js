323158b1947bf2f65c92d29a8d302a83f1bf4f02
Ahsan Ghazni Khan <ahsan123pk@gmail.com>
2022-11-01 21:28:04 +0500

    js file

--- books.js
+++ books.js
@@ -1,8 +1,34 @@
 const books = [];
+const bookList = document.querySelector('.book_list');
+
+//function to get local storage
+function populate() {
+  //retrieving local storage
+  const stored = localStorage.booksData
+  const prsd = JSON.parse(stored)
+  if(stored) {
+   for(var i=0;i<prsd.length;i++) {
+    // retrieving from local storage
+    books.push(prsd[i])
+    var li = document.createElement('li');
+    var p = document.createElement('p');
+    p.innerHTML = `${books[i].bookTitle}<br>${books[i].bookAuthor}
+    <br><button type="button" class="remove${i}" id="remove">Remove</button><hr>`;
+    li.appendChild(p);
+    bookList.appendChild(li);
+    const removeBtn = document.querySelector(`.remove${books.length - 1}`);
+    removeBtn.addEventListener('click', remove);
+   }
+  }
+}
+document.addEventListener('DOMContentLoaded', populate); //function to call on load
 
 function remove(button) {
   button.target.parentElement.parentElement.remove();
-  delete books[button.target.className.substring(6)];
+  books.splice(button.target.className.substring(6),1);
+
+  // again storing the array in local storage
+  localStorage.setItem('booksData', JSON.stringify(books));
 }
 
 function add() {
@@ -12,26 +38,23 @@ function add() {
   const book = {
     bookTitle: title,
     bookAuthor: author,
-    bookID: books.length,
+    bookID: books.length
   };
 
   books.push(book);
-  const bookList = document.querySelector('.book_list');
   const li = document.createElement('li');
   const p = document.createElement('p');
   p.innerHTML = `${books[books.length - 1].bookTitle}<br>${books[books.length - 1].bookAuthor}
     <br><button type="button" class="remove${books.length - 1}" id="remove">Remove</button><hr>`;
   li.appendChild(p);
   bookList.appendChild(li);
-
+  
+  if(typeof(Storage) !== 'undefined') {
   // storing in local storage
   // converting all array data into string
   const json = JSON.stringify(books);
-  localStorage.formData = json;
-
-  // retrieving from local storage
-  const stored = localStorage.getItem('formData');
-  const prsd = JSON.parse(stored);
+  localStorage.setItem('booksData', json)
+}
 
   const removeBtn = document.querySelector(`.remove${books.length - 1}`);
   removeBtn.addEventListener('click', remove);
